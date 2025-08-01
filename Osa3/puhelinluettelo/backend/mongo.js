8051c575ecc85c207873b7f8f25e6144f2d1b2a4
hartonenolli <hartonenolli@gmail.com>
2025-08-01 10:43:14 +0300

    3.12: tietokanta komentoriviltä

---
+++ Osa3/puhelinluettelo/backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://hartonenolli:${password}@cluster0.3irzhka.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+    console.log('Täällä');
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    console.log('adding person:', person.name, person.number)
+    person.save().then(() => {
+        mongoose.connection.close()
+    })
+}
+
+
