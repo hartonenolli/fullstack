1ee822160a6408866274cba96e0de4abe5741abc
hartonenolli <hartonenolli@gmail.com>
2025-08-04 12:40:03 +0300

    3.17*: puhelinluettelo ja tietokanta, step5

--- Osa3/puhelinluettelo/backend/index.js
+++ Osa3/puhelinluettelo/backend/index.js
@@ -4,11 +4,12 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-morgan('tiny')
 morgan.token('body', (req) => JSON.stringify(req.body))
+const logger = morgan('tiny', {skip: (req, res) => req.method !== 'POST'})
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
+app.use(logger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
@@ -56,15 +57,21 @@ app.get('api/persons/:id', (request, response, next) => {
         response.status(400).send({ error: 'malformatted id'})
 })
 
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const person = persons.find(p => p.id === id)
-//     if (person) {
-//         response.json(person)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => {
+            next(error)
+        })
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
