dde3df180d3abba95df2c8bf7ad999956da50478
hartonenolli <hartonenolli@gmail.com>
2025-08-27 13:50:47 +0300

    6.5: anekdootit, step3

--- Osa6/redux-anecdotes/src/App.jsx
+++ Osa6/redux-anecdotes/src/App.jsx
@@ -17,10 +17,12 @@ const App = () => {
     console.log('add anecdote', content)
     dispatch(addAnecdote(content))
   }
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
