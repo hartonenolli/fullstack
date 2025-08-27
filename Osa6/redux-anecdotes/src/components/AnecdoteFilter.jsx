b621f421dfee913cb18418fc1dfb0b16a5cc377a
hartonenolli <hartonenolli@gmail.com>
2025-08-27 15:54:04 +0300

    6.9 paremmat anekdootit, step7, needs work on filter function

---
+++ Osa6/redux-anecdotes/src/components/AnecdoteFilter.jsx
@@ -0,0 +1,19 @@
+import { filterChange } from '../reducers/filterReducer.js'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteFilter = () => {
+  const dispatch = useDispatch()
+  const handleChange = (event) => {
+    dispatch(filterChange(event.target.value))
+  }
+  const style = {
+    marginBottom: 10
+  }
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default AnecdoteFilter
--- Osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -12,10 +12,13 @@ const AnecdoteForm = () => {
   };
 
   return (
-    <form onSubmit={addNewAnecdote}>
-      <div><input name="anecdote" /></div>
-      <button type="submit">create</button>
-    </form>
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addNewAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </>
   );
 };
 
