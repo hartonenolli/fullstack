bf9162183570f0bbba18965f28d6c51054cc4130
hartonenolli <hartonenolli@gmail.com>
2026-03-22 12:35:08 +0200

    8.8: Kirjailijoiden näkymä

---
+++ Osa8/kirjasto-frontend/src/components/Authors.jsx
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = (props) => {
+  const { loading, error, data } = useQuery(ALL_AUTHORS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  const authors = data.allAuthors
+
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
+
