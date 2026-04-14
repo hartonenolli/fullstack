5776d28a4ddc889a27e1e0e1cc0f98ad989749a1
hartonenolli <hartonenolli@gmail.com>
2026-04-14 22:25:30 +0300

    8.25: Subscriptionit client, osa 2

--- Osa8/kirjasto-frontend/src/App.jsx
+++ Osa8/kirjasto-frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useSubscription, useApolloClient } from '@apollo/client/react'
+import { useSubscription, useApolloClient, useQuery } from '@apollo/client/react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Recommend from './components/Recomend'
 import LoginForm from './components/LoginForm'
-import { BOOK_ADDED, ALL_BOOKS } from './queries'
+import { BOOK_ADDED, ALL_BOOKS, GENRES } from './queries'
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('library-user-token'))
@@ -18,17 +18,25 @@ const App = () => {
 
   const client = useApolloClient()
 
+  useQuery(GENRES)
   useSubscription(BOOK_ADDED, {
     onData: ({ data }) => {
       const addedBook = data.data.bookAdded
       alert(`New book added: ${addedBook.title} by ${addedBook.author.name}`)
-      const dataInStore = client.readQuery({ query: ALL_BOOKS })
-      if (!dataInStore.allBooks.find(b => b.id === addedBook.id)) {
-        client.writeQuery({
-          query: ALL_BOOKS,
-          data: { allBooks: dataInStore.allBooks.concat(addedBook) },
-        })
-      }
+
+      client.cache.updateQuery({ query: ALL_BOOKS }, (dataInStore) => {
+        if (!dataInStore) return dataInStore
+        if (dataInStore.allBooks.find(b => b.id === addedBook.id)) return dataInStore
+        return { allBooks: dataInStore.allBooks.concat(addedBook) }
+      })
+
+      client.cache.updateQuery({ query: GENRES }, (dataInStore) => {
+        if (!dataInStore) return dataInStore
+        const updatedGenres = Array.from(
+          new Set([...dataInStore.genres, ...addedBook.genres])
+        )
+        return { genres: updatedGenres }
+      })
     }
   })
 
